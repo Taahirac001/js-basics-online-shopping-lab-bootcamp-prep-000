795c9818b322ed5600704ce93f33bd9eb10df0f2
Taahira Carter <taahirac001@gmail.com>
2019-11-06 03:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,41 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var objects = {itemName:`${item}`, itemPrice: Math.floor(Math.random()*100)};
+ cart.push(objects);
+ return `${objects.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  var newCart = [];
+  if(cart.length === 0){
+    return "Your shopping cart is empty."
+  } else{
+    for(var i = 0; i < cart.length; i++){
+      var itemAndPrice = `${cart[i]['itemName']} at \$${cart[i]['itemPrice']}`;
+     newCart.push(itemAndPrice);
+     var string = 'In your cart, you have ';
+     if(newCart.length === 1){
+       string += newCart.toString();
+       string += '.'
+       return string;
+     }else if(newCart.length === 2){
+       string += newCart.toString().join(', and ');
+       string += '.';
+       return string;
+     }
+    }
+  }
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  for(var i = 0; i < cart.length; i++){
+    total += cart[i]['itemPrice'];
+    return total;
+    }
 }
 
 function removeFromCart(item) {
